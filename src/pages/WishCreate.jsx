721eef235cca29cb46b66694a0a3bad5b7164d04
GiHwan2 <sandwich5011@naver.com>
2024-08-26 02:07:22 +0900

    feat: 소원 달기 기능 구현

--- src/pages/WishCreate.jsx
+++ src/pages/WishCreate.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { createWish } from "../apis/wishes";
+import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
   display: flex;
@@ -61,26 +63,60 @@ const SubmitButton = styled.button`
 `;
 
 function WishCreate() {
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("진로");
+  const [content, setContent] = useState("");
+  const navigate = useNavigate(); // useNavigate 훅 사용
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const wishData = {
+      title,
+      content,
+      category,
+    };
+
+    try {
+      const response = await createWish(wishData);
+      console.log("Wish created:", response);
+      navigate("/"); // 소원 등록 후 "/" 페이지로 이동
+    } catch (error) {
+      console.error("Error creating wish:", error);
+    }
+  };
+
   return (
     <Container>
-      <Label>소원 제목</Label>
-      <Input type="text" placeholder="소원 제목을 입력하세요" />
+      <form onSubmit={handleSubmit}>
+        <Label>소원 제목</Label>
+        <Input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="소원 제목을 입력하세요"
+        />
 
-      <Label>소원 카테고리</Label>
-      <Select>
-        <option value="진로">진로</option>
-        <option value="건강">건강</option>
-        <option value="인간 관계">인간 관계</option>
-        <option value="돈">돈</option>
-        <option value="목표">목표</option>
-        <option value="학업/성적">학업/성적</option>
-        <option value="기타">기타</option>
-      </Select>
+        <Label>소원 카테고리</Label>
+        <Select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="진로">진로</option>
+          <option value="건강">건강</option>
+          <option value="인간 관계">인간 관계</option>
+          <option value="돈">돈</option>
+          <option value="목표">목표</option>
+          <option value="학업/성적">학업/성적</option>
+          <option value="기타">기타</option>
+        </Select>
 
-      <Label>소원 본문</Label>
-      <Textarea placeholder="소원 본문을 입력하세요" />
+        <Label>소원 본문</Label>
+        <Textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="소원 본문을 입력하세요"
+        />
 
-      <SubmitButton>소원 등록</SubmitButton>
+        <SubmitButton type="submit">소원 등록</SubmitButton>
+      </form>
     </Container>
   );
 }

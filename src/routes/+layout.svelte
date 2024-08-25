8bc3b53d500e736e94ddd2e284142dc10a7e804c
j._.jinoo <jinoo0306@naver.com>
2024-08-25 16:05:17 +0900

    feat : 헤더 컴포넌트 기능 구현

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,41 @@
-<script>
-	import Header from './Header.svelte';
-	import '../app.css';
+<script lang="ts">
+	import '../app.scss';
+	import Header from './_components/Header.svelte';
 </script>
 
-<div class="app">
+<div class="layout">
 	<Header />
-
 	<main>
 		<slot />
 	</main>
 </div>
+
+<style lang="scss">
+	.layout {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+		header {
+			background-color: $primary-color;
+			padding: 1rem;
+			color: white;
+			nav {
+				a {
+					color: white;
+					margin-right: 1rem;
+					text-decoration: none;
+					&:hover {
+						text-decoration: underline;
+					}
+				}
+			}
+		}
+		footer {
+			margin-top: auto;
+			background-color: $secondary-color;
+			padding: 1rem;
+			text-align: center;
+			color: white;
+		}
+	}
+</style>
---
+++ src/routes/_components/Header.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	let permissions: string;
+	let targetUrl: string;
+
+	onMount(() => {
+		checkPermissions();
+	});
+
+	// 페이지 로드 시 localStorage에서 permissions 값을 확인
+	function checkPermissions() {
+		permissions = localStorage.getItem('permissions') || '';
+		if (permissions === 'admin') {
+			targetUrl = '/wish-fruit/allow';
+		} else {
+			targetUrl = '/wish';
+		}
+	}
+
+	function handleKeyDown(event: KeyboardEvent, url: string) {
+		if (event.key === 'Enter' || event.key === ' ') {
+			goto(url);
+		}
+	}
+</script>
+
+<header>
+	<nav>
+		<button
+			class="title"
+			tabindex="0"
+			aria-label="Go to Home"
+			on:click={() => goto('/')}
+			on:keydown={(event) => handleKeyDown(event, '/')}
+		>
+			Techeer Tree
+		</button>
+		<button
+			aria-label="Go to About"
+			on:click={() => goto(targetUrl)}
+			on:keydown={(event) => handleKeyDown(event, '/about')}
+		>
+			{permissions === 'admin' ? '소원 승인 하기' : '소원 열매 달기'}
+		</button>
+	</nav>
+</header>
+
+<style lang="scss">
+	header {
+		background-color: $primary-color;
+		color: white;
+		height: 3.75rem;
+		display: flex;
+		align-items: center;
+
+		nav {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			padding: 0 1rem;
+			width: 100%;
+		}
+
+		.title {
+			font-size: 1.5rem;
+			font-weight: bold;
+		}
+	}
+</style>

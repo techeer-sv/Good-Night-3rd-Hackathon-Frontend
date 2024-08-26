4e8c45afacda07a66664391860a698dfb42a8b21
kingmusung <abuga991008@gmail.com>
2024-08-26 17:11:00 +0900

    소원조회 퍼블리싱

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import WishTreePage from './page/WishTreePage.tsx';
 import WishAddPage from './page/WishAddPage.tsx';
+import WishGetPage from './page/WishGetPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <WishTreePage></WishTreePage> */}
-    <WishAddPage></WishAddPage>
+    {/* <WishAddPage></WishAddPage> */}
+    <WishGetPage></WishGetPage>
   </StrictMode>,
 );
---
+++ frontend/src/page/WishGetPage.tsx
@@ -0,0 +1,20 @@
+import react from '@vitejs/plugin-react-swc';
+import BackLayout from '../component/wishget/Backlayout';
+import NavBar from '../component/NavBar';
+
+const WishGetPage: React.FC = () => {
+  return (
+    <BackLayout>
+      <NavBar></NavBar>
+      <div className="m-auto mt-[17vh] w-[30%] h-[50%] flex flex-col justify-center">
+        <p className="text-[32px] font-bold">취업 잘 되게 해주세요</p>
+        <p className="text-[16px] ">진로</p>
+        <p className="text-[20px] font-bold">
+          ㅁㄴ엄ㄴ어ㅏ안머아ㅓㅁ너ㅏㅁㄴ어ㅏㅇㄴ마ㅓdd,ld
+        </p>
+      </div>
+    </BackLayout>
+  );
+};
+
+export default WishGetPage;

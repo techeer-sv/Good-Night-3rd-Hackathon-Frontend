b4f9047960e2dd4d09d9529743c25e5353220039
dongmin <dongmin11566@gmail.com>
2024-08-29 14:06:41 +0900

    refactor: 다수의 상태를 reducer로 관리

--- frontend/src/app/_component/WishSection.tsx
+++ frontend/src/app/_component/WishSection.tsx
@@ -1,17 +1,13 @@
 'use client';
 
-import { useState, useEffect, useRef } from 'react';
+import { useEffect, useReducer } from 'react';
 import WishFruit from '@/app/_component/WishFruit';
 import { getWishes } from '@/app/_lib/getWishes';
-import { Wish } from '@/model/Wish';
 import InfiniteScroll from '@/app/_component/InfiniteScroll';
+import { initialState, reducer } from '@/app/_lib/reducer';
 
 export default function WishSection() {
-  const [category, setCategory] = useState<string>(''); // 선택된 카테고리를 저장할 상태
-  const [wishes, setWishes] = useState<Wish[]>([]); // 소원 목록
-  const [page, setPage] = useState<number>(1); // 현재 페이지
-  const [isLoading, setIsLoading] = useState<boolean>(false); // 데이터 로딩 상태
-  const [hasMore, setHasMore] = useState<boolean>(true); // 추가 데이터 여부
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const categories = [
     { value: '', label: '모든 카테고리' },
@@ -26,38 +22,35 @@ export default function WishSection() {
 
   useEffect(() => {
     const fetchWishes = async () => {
-      setIsLoading(true);
+      dispatch({ type: 'SET_LOADING', isLoading: true });
 
       try {
         const newWishes = await getWishes(
           'true',
-          category,
-          page.toString(),
+          state.category,
+          state.page.toString(),
           '3',
         );
         if (newWishes) {
-          setWishes((prevWishes: Wish[]) => [...prevWishes, ...newWishes]);
-          setHasMore(newWishes.length > 0); // 더 가져올 데이터가 있는지 확인
+          dispatch({ type: 'FETCH_SUCCESS', wishes: newWishes });
+          dispatch({ type: 'SET_HAS_MORE', hasMore: newWishes.length > 0 });
         }
       } catch (error) {
         console.error('소원 데이터를 불러오는 중 오류가 발생했습니다:', error);
       } finally {
-        setIsLoading(false); // 에러가 발생해도 로딩 상태를 false로 설정
+        dispatch({ type: 'SET_LOADING', isLoading: false });
       }
     };
 
     fetchWishes();
-  }, [category, page]);
+  }, [state.category, state.page]);
 
   useEffect(() => {
-    // 카테고리가 변경될 때 페이지와 소원 목록 초기화
-    setPage(1);
-    setWishes([]);
-    setHasMore(true);
-  }, [category]);
+    dispatch({ type: 'RESET_WISHES' });
+  }, [state.category]);
 
   const loadMore = () => {
-    setPage((prevPage) => prevPage + 1);
+    dispatch({ type: 'INCREMENT_PAGE' });
   };
 
   return (
@@ -67,8 +60,10 @@ export default function WishSection() {
           id="category"
           name="category"
           className="text-yellow-300 mt-1 border-none glass bg-yellow-500 bg-opacity-50 block w-fit p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
+          value={state.category}
+          onChange={(e) =>
+            dispatch({ type: 'SET_CATEGORY', category: e.target.value })
+          }
         >
           {categories.map((item) => (
             <option key={item.value} value={item.value}>
@@ -79,11 +74,11 @@ export default function WishSection() {
       </div>
       <InfiniteScroll
         loadMore={loadMore}
-        hasMore={hasMore}
-        isLoading={isLoading}
+        hasMore={state.hasMore}
+        isLoading={state.isLoading}
       >
         <div className="grid grid-cols-3 gap-8 overflow-y-scroll h-[60vh]">
-          {wishes.map((wish, index) => (
+          {state.wishes.map((wish, index) => (
             <div
               key={wish.id}
               className="flex flex-col justify-between min-h-[calc(60vh/2-2rem)]"
